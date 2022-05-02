c89cb7a812257d1afcc10eb0575f88c17149eca7
Kaliom Geefker <85347571+Ludo-Kaliom@users.noreply.github.com>
2022-05-02 15:47:37 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php 
 // Configuration de la connexion
-define('DB_HOST','localhost');
-define('DB_USER','root');
+define('DB_HOST','');
+define('DB_USER','');
 define('DB_PASS','');
 define('DB_NAME','library');
 
